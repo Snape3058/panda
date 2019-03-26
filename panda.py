d1ddb93a9056360695f3404488c061011b780707
Ella <maxutong16@otcaix.iscas.ac.cn>
2019-03-26 15:51:52 +0800

    finish implementing all new arguments: cfm, cp

--- panda.py
+++ panda.py
@@ -6,19 +6,34 @@ import json
 import threadpool
 import argparse
 from subprocess import Popen as popen
+from subprocess import PIPE as pipe
+import re
 
 
 # default configurations and strings
-class Default:
+class Default:  # {{{
     # configurable names, tags, and other strings
     ast = 'Clang PCH file'
     i = 'C/C++ preprocessed file'
     ll = 'LLVM IR file'
     bc = 'LLVM BitCode file'
     fm = 'Clang function-mapping file'
+    filterstr = [
+            '-o:',
+            '-O([0123sg]|fast)?',
+            '-Werror(=.+)?',
+            '-W(all|extra)?',
+            '-fsyntax-only',
+            '-g',
+            '.+\\.o(bj)?',
+            ]
     Version = '2.0'
     Commit = '%REPLACE_COMMIT_INFO%'
     Now = '%REPLACE_NOW%'
+    cc = 'clang'
+    cxx = 'clang++'
+    cfm = 'clang-func-mapping'
+    fmname = 'externalFnMap.txt'
 
     # program description
     DescriptionMsg = '''Generate preprocessed files from compilation database.
@@ -39,9 +54,11 @@ Provided by REST team, ISCAS.
 Copyright 2018-{}. All rights reserved.'''.format(
             Version, Commit, Now)
 
+    # }}}
+
 
 # ParseArguments: parse command line arguments with argparse.
-def ParseArguments(args):
+def ParseArguments(args):  # {{{
     parser = argparse.ArgumentParser(description=Default.DescriptionMsg,
             formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-v', '--version', action='version', version=Default.VersionMsg)
@@ -66,133 +83,131 @@ def ParseArguments(args):
             type=str, dest='input', default=os.path.abspath('./compile_commands.json'),
             help='Customize the compilation database file.')
     parser.add_argument('-c', '--cc',
-            type=str, dest='cc', default='clang',
+            type=str, dest='cc', default=Default.cc,
             help='Customize the C compiler. (default is clang)')
     parser.add_argument('-C', '--cxx',
-            type=str, dest='cxx', default='clang++',
+            type=str, dest='cxx', default=Default.cxx,
             help='Customize the C++ compiler. (default is clang++)')
-    parser.add_argument('-m', '--fnmapping',
-            type=str, dest='fnmapping', default='clang-func-mapping',
+    parser.add_argument('-m', '--cfm',
+            type=str, dest='cfm', default=Default.cfm,
             help='Customize the function mapping scanner. (default is clang-func-mapping)')
+    parser.add_argument('-p', '--clang-path', metavar='CLANG_PATH', type=str, dest='clang',
+            help='Customize the Clang executable directory for searching compilers.')
     parser.add_argument('-j', '--jobs', type=int, dest='jobs', default=1,
             help='Customize the number of jobs allowed in parallel.')
-    parser.add_argument('--dump', action='store_true', dest='dump',
+    parser.add_argument('--dump-only', action='store_true', dest='dump_only',
             help='Generate and dump commands to stdout only.')
     parser.add_argument('--ctu', action='store_true', dest='ctu',
             help='Alias to -M -A -P.')
-    return parser.parse_args(args[1:])
+    opts = parser.parse_args(args[1:])
+
+    if opts.clang:
+        if Default.cc == opts.cc:
+            opts.cc = os.path.abspath(os.path.join(opts.clang, Default.cc))
+        if Default.cxx == opts.cxx:
+            opts.cxx = os.path.abspath(os.path.join(opts.clang, Default.cxx))
+        if Default.cfm == opts.cfm:
+            opts.cfm = os.path.abspath(os.path.join(opts.clang, Default.cfm))
+
+    if opts.ctu:
+        opts.fm = True
+        opts.ast = True
+        opts.cp = True
+
+    return opts
+
+    # }}}
 
 
-# RecoverOriginalFileName: recover the original absolute file name in command
+# GetSourceFile: find out source file path through command object
 #
-#   directory: the directory where the compilation happened
-#           e.g. json['directory']
-#   filename: the filename to be parsed (can be both relative and absolute)
-#           e.g. input file (json['file']), output file (-o value)
-#                include directory (-I value)
-#
-#   return: if filename is relative path, then concatenate them
-#           if is absolute path, then drop directory and return filename directly
-def RecoverOriginalFileName(directory, filename):
-    return os.path.abspath(os.path.join(directory, filename))
-
-
-# GenerateCompiler: generate the preprocessor compiler
-#
-#   command: the original command object to be processed
-#
-#   return: the preprocessor compiler
-def GenerateCompiler(opts, command):
-    # check suffix only as the compiler argument can be full path
-    if 'cc' == command['arguments'][0][-2:]:
-        return opts.cc
-    elif 'c++' == command['arguments'][0][-3:]:
-        return opts.cxx
-    else:
-        assert False, 'What is this compiler? ' + command['arguments'][0]
-
-
-# GenerateOutput: generate the full path of output file with file type suffix
-#
-#   outputDir: the output argument provided in command arguments
-#   originalOutput: the recovered output filename
-#   suffix: the suffix representing its file type to replace '.o'
-#
-#   return: the absolute path of the output file in format:
-#           /outputdir/absolute/path/to/output/file.suffix
-def GenerateOutput(outputDir, workspace, originalOutput, suffix):
-    recoveredOutput = RecoverOriginalFileName(workspace, originalOutput)
-    return os.path.join(outputDir, recoveredOutput[1:]) + '.' + suffix
+#   command: a compile command object in database
+def GetSourceFile(command):
+    return os.path.abspath(os.path.join(command['directory'], command['file']))
 
 
 # MakeCommand: replace the arguments with correct value for preprocess
 #
 #   opts: opts object (refer to ParseArguments)
 #   command: the command object parsed from JSON
-#   suffix: the suffix representing its file type to replace '.o'
-#   additional: the additional arguments to do the corresponding preprocess
+#   extension: the suffix representing its file type
+#   prefix, suffix: the additional arguments to do the corresponding preprocess
+#   argfilter: function to filter arguments
 #
 #   return: command object
 #       the command object is defined as follows:
 #           - arguments: list of arguments to be executed by compiler
 #           - directory: compiler working directory
 #           - output: directory of -o parameter
-def MakeCommand(opts, command, suffix, additional, keptArgs):
-    arguments = [GenerateCompiler(opts, command)]
+def MakeCommand(opts, command, extension, prefix, suffix, argfilter):
+    # GenerateCompiler: generate the preprocessor compiler
+    def GenerateCompiler(compiler):
+        # check suffix only as the compiler argument can be full path
+        if 'cc' == compiler[-2:]:
+            return opts.cc
+        elif 'c++' == compiler[-3:]:
+            return opts.cxx
+        else:
+            assert False, 'What is this compiler? ' + compiler
+    arguments = [GenerateCompiler(command['arguments'][0])]
 
     # append additional arguments for generating targets
-    arguments += additional
+    arguments += prefix
 
-    # generate arguments
-    i = iter(command['arguments'])
-    while True:
-        A = next(i, None)
-        if A is None:
-            break
+    # generate arguments (including the source file)
+    # FIXME: when generating with both source files and object files,
+    #        if object files are not available, clang will report 404.
+    #        Currently, a filter is added to remove all .o or .obj files.
+    arguments += argfilter(command['arguments'][1:])
 
-        # remove output argument if it has
-        if '-o' == A:
-            A = next(i)
-
-        # generate the argument should be kept
-        else:
-            for kept in keptArgs:
-                head = A[:len(kept)]
-                if kept == head:
-                    if head == A:
-                        arguments.append(A + next(i))
-                    else:
-                        arguments.append(A)
-
-    # append output argument
-    output = GenerateOutput(opts.output, command['directory'],
-            command['file'], suffix)
+    # generate the full path of output file with file type extension
+    output = os.path.join(opts.output, GetSourceFile(command)[1:]) + '.' + extension
     arguments += ['-o', output]
 
-    # append input argument
-    arguments.append(RecoverOriginalFileName(
-        command['directory'], command['file']))
+    # append additional arguments for generating targets
+    arguments += suffix
 
     return {'directory': command['directory'],
             'arguments': arguments,
             'output': output}
 
 
-# RunCommand: run the command to do the preprocess
+# MakeCopyCommand: make command for copy operation.
 #
 #   opts: opts object (refer to ParseArguments)
+#   command: the command object parsed from JSON
+#
+#   return: command object of cp $file $output/$directory/$file
+def MakeCopyCommand(opts, command):
+    src = GetSourceFile(command)
+    output = os.path.join(opts.output, src[1:])
+
+    return {'directory': command['directory'],
+            'arguments': ['cp', src, output],
+            'output': output}
+
+
+# RunCommand: run the command to do the preprocess
+#
 #   command: the command object to be executed. (in the parsed JSON format)
-def RunCommand(opts, command):
-    print('Generating "' + command['output'] + '"')
+#   verbose: dump the command to be executed.
+#   dump_only: dump and exit, do not execute.
+def RunCommand(command, verbose, dump_only):
+    if not dump_only:
+        print('Generating "' + command['output'] + '"')
 
     arguments = command['arguments']
-    if opts.verbose or opts.dump:
+    outputDir = os.path.dirname(command['output'])
+
+    if dump_only:
+        print("mkdir -p " + outputDir)
+        print("cd " + command['directory'])
+        print(" \\\n\t".join(arguments))
+        return
+    elif verbose:
         print(arguments)
 
-    if opts.dump:
-        return
-
-    outputDir = os.path.dirname(command['output'])
+    # create directory for output file
     if not os.path.exists(outputDir):
         try:
             os.makedirs(outputDir)
@@ -203,36 +218,120 @@ def RunCommand(opts, command):
     process.wait()
 
 
+# GenerateFunctionMappingList: invoke clang-func-mapping to
+#   generate externalFnMap.txt
+#
+#   opts: opts object (refer to ParseArguments)
+#   jobs: the compilation database
+def GenerateFunctionMappingList(opts, jobs):
+    if not opts.dump_only:
+        print('Generating function mapping list.')
+
+    src = [GetSourceFile(i) for i in jobs]
+    path = os.path.dirname(opts.input)
+    arguments = [opts.cfm, '-p', path] + src
+    outfile = os.path.join(opts.output, Default.fmname)
+
+    if opts.dump_only:
+        print(" \\\n\t".join(arguments) + " | \\\n" +
+                "\tsed 's/$/.ast/g' >" + outfile)
+        return
+
+    process = popen(arguments, stdout=pipe, stderr=pipe)
+    (out, err) = process.communicate()
+    fm = out.decode('utf-8').replace('\n', '.ast\n')
+
+    with open(outfile, 'w') as fout:
+        fout.write(fm)
+
+    process.wait()
+
+
 # PreprocessProject: monitor and control the process of preprocess
 #
 #   opts: opts object (refer to ParseArguments)
 def PreprocessProject(opts):
-    def jobRun(opts, job):
+    def jobRun(opts, job):  # {{{
+        commands = []
+
+        # compile filter regexp and return a filter of it
+        #   filterstr: a list of filter strings
+        def getArgFilter(filterstr):  # {{{
+            # create filter
+            filters = []
+
+            for fs in filterstr:
+                if ':' == fs[-1]:
+                    filters.append((re.compile(fs[:-1]), True))
+                else:
+                    filters.append((re.compile(fs), False))
+
+            def ArgFilter(argv):
+                ret = []
+
+                I = iter(argv)
+                while True:
+                    A = next(I, None)
+                    if A is None:
+                        break
+
+                    for f in filters:
+                        # remove matched
+                        if f[0].fullmatch(A):
+                            if f[1]:
+                                next(I)
+                            A = None
+                            break
+
+                    if A:
+                        ret.append(A)
+
+                return ret
+
+            return ArgFilter
+            # }}}
+
+        # jobRun:
         if opts.ast:
-            RunCommand(opts, MakeCommand(
-                opts, job, 'ast', ['-emit-ast'],
-                ['-std', '-D', '-U', '-I']))
+            commands.append(MakeCommand(
+                opts, job, 'ast', ['-emit-ast'], ['-w'],
+                getArgFilter(Default.filterstr)))
 
         if opts.i:
-            RunCommand(opts, MakeCommand(
-                opts, job, 'i', ['-E'],
-                ['-std', '-D', '-U', '-I']))
+            commands.append(MakeCommand(
+                opts, job, 'i', ['-E'], ['-w'],
+                getArgFilter(Default.filterstr)))
 
         if opts.ll:
-            RunCommand(opts, MakeCommand(
-                opts, job, 'll', ['-c', '-g', '-emit-llvm', '-S'],
-                ['-std', '-D', '-U', '-I', '-f', '-m']))
+            commands.append(MakeCommand(
+                opts, job, 'll', ['-c', '-g', '-emit-llvm', '-S'], ['-w'],
+                getArgFilter(Default.filterstr)))
 
         if opts.bc:
-            RunCommand(opts, MakeCommand(
-                opts, job, 'bc', ['-c', '-g', '-emit-llvm'],
-                ['-std', '-D', '-U', '-I', '-f', '-m']))
+            commands.append(MakeCommand(
+                opts, job, 'bc', ['-c', '-g', '-emit-llvm'], ['-w'],
+                getArgFilter(Default.filterstr)))
 
+        if opts.cp:
+            commands.append(MakeCopyCommand(opts, job))
+
+        for cmd in commands:
+            RunCommand(cmd, opts.verbose, opts.dump_only)
+
+        # }}}
+
+    # PreprocessProject:
     if not os.path.exists(opts.output):
         os.makedirs(opts.output)
 
     jobList = json.load(open(opts.input, 'r'))
 
+    # Do sequential job:
+    # Generate function mapping list
+    if opts.fm:
+        GenerateFunctionMappingList(opts, jobList)
+
+    # Do parallel job:
     if 1 == opts.jobs:
         for i in jobList:
             jobRun(opts, i)
